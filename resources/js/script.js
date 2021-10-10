06cee309a86e03f845da4e1b427ebc10c25f4276
isomoth <ic.gonzalez35@hotmail.com>
2021-10-10 19:08:32 +0200

    fixed horizontal scroll on mobile

---
+++ resources/js/script.js
@@ -0,0 +1,5 @@
+/* const all = document.getElementsByTagName = '*'
+
+const checkForBig = $.each($('*'), function() { if ($(this).width() > $('body').width()) { console.log($(this).get(0)); } }).length;
+
+console.log(checkForBig) */
