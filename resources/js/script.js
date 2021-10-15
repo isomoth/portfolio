2b864a9dda4ef47ecf31ed44c9164db70c39fec3
isomoth <ic.gonzalez35@hotmail.com>
2021-10-15 22:32:51 +0200

    fixed overlay and added variables for the color palette

--- resources/js/script.js
+++
@@ -1,5 +0,0 @@
-/* const all = document.getElementsByTagName = '*'
-
-const checkForBig = $.each($('*'), function() { if ($(this).width() > $('body').width()) { console.log($(this).get(0)); } }).length;
-
-console.log(checkForBig) */
